d324671cf4539d032cb90d42e1a7c91af9289625
Yuval Dolev <yuvaldolev2@gmail.com>
2020-04-03 15:56:59 +0300

    Added MacOS build script.

---
+++ scripts/build_mac.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# If any command errors, stop the script.
+set -e
+
+# Set up directories.
+ME=$(realpath "$0")
+LOCATION=$(dirname "$ME")
+PRJ_ROOT=$(dirname "$LOCATION")
+SRC_ROOT="$PRJ_ROOT/src"
+INTERMEDIATE_ROOT="$PRJ_ROOT/.build"
+
+mkdir -p "$INTERMEDIATE_ROOT"
+
+# Get the build mode.
+BUILD_MODE="$1"
+if [ -z "$BUILD_MODE" ]; then
+	BUILD_MODE="-DBUILD_DEV"
+fi
+
+CC=clang
+WARNINGS="-Wall -Werror"
+COMPILER_FLAGS="-g -fPIC -fpermissive $BUILD_MODE"
+
+# Execute
+$CC $WARNINGS $COMPILER_FLAGS "$SRC_ROOT/build_compiler.c" -o "$INTERMEDIATE_ROOT/build_compiler"
+pushd "$PRJ_ROOT"
+"$INTERMEDIATE_ROOT/build_compiler"
+popd
+
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, const char * const argv[])
+{
+	printf("Compiling...\n");
+}
